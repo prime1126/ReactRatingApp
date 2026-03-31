92b4d2c43c556e12d383c9b703d3aec2060b95aa
prime1126 <valdezjonah111@gmail.com>
2026-03-31 14:59:38 -0500

    finished the functionality for the component to track counting likes and dislikes as well as total likes

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,11 +5,34 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    //initialize the states for likes and dislikes assuming this is the parent comp.
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      totalRatings: 0,
+      handleLike: () => 
+      {
+        this.setState((prevState) => ({likes: prevState.likes +1, totalRatings: prevState.totalRatings+1}));
+      },
+      handleDislike: () => 
+      {
+        this.setState((prevState) => ({dislikes: prevState.dislikes +1, totalRatings: prevState.totalRatings+1}));
+      }
+    };
   }
+
   render() {
     return (
+      //assuming the div content-rating is the main div
      <>
-     <h1>Text Content Rating</h1>
+     <div className='content-rating'>
+      <p>Dogs are mans best friend</p>
+      <div className='rating-buttons'>
+        <button name='like-button' onClick={this.state.handleLike}>Like({this.state.likes})</button>
+        <button name='dislike-button'onClick={this.state.handleDislike}>Dislike({this.state.dislikes})</button>
+        <p>Total number of Likes+Dislikes({this.state.totalRatings})</p>
+      </div>
+     </div>
      </>
     );
   }
